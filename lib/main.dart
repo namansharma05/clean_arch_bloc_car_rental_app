d01eacf4cdaf818832b4b4964978731a0081c2b3
Naman Sharma <namanks0511@gmail.com>
2024-10-12 17:32:03 +0530

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:clean_arch_bloc_car_rental_app/firebase_options.dart';
+import 'package:clean_arch_bloc_car_rental_app/injection_container.dart';
+import 'package:clean_arch_bloc_car_rental_app/presentation/bloc/car_bloc.dart';
+import 'package:clean_arch_bloc_car_rental_app/presentation/bloc/car_event.dart';
 import 'package:clean_arch_bloc_car_rental_app/presentation/pages/onboarding_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  init();
   runApp(const MyApp());
 }
 
@@ -17,14 +22,23 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => CarBloc(
+            getCars: getIt(),
+          )..add(LoadCars()),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const OnboardingPage(),
       ),
-      home: const OnboardingPage(),
     );
   }
 }
--- lib/presentation/pages/car_list_screen.dart
+++ lib/presentation/pages/car_list_screen.dart
@@ -1,15 +1,11 @@
-import 'package:clean_arch_bloc_car_rental_app/data/models/car.dart';
+import 'package:clean_arch_bloc_car_rental_app/presentation/bloc/car_bloc.dart';
+import 'package:clean_arch_bloc_car_rental_app/presentation/bloc/car_state.dart';
 import 'package:clean_arch_bloc_car_rental_app/presentation/widgets/car_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CarListScreen extends StatelessWidget {
-  final List<Car> cars = [
-    Car(model: "Fortuner", distance: 870, fuelCapacity: 50, pricePerHour: 45),
-    Car(model: "Fortuner", distance: 870, fuelCapacity: 50, pricePerHour: 45),
-    Car(model: "Fortuner", distance: 870, fuelCapacity: 50, pricePerHour: 45),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,12 +15,28 @@ class CarListScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         foregroundColor: Colors.black,
       ),
-      body: ListView.builder(
-        itemCount: cars.length,
-        itemBuilder: (context, index) {
-          return CarCard(
-            car: cars[index],
-          );
+      body: BlocBuilder<CarBloc, CarState>(
+        builder: (context, state) {
+          if (state is CarLoadingState) {
+            return const Center(
+              child: CupertinoActivityIndicator(),
+            );
+          } else if (state is CarLoadedState) {
+            return ListView.builder(
+              itemCount: state.cars!.length,
+              itemBuilder: (context, index) {
+                return CarCard(
+                  car: state.cars![index],
+                );
+              },
+            );
+          } else if (state is CarErrorState) {
+            return Center(
+              child: Text(state.errorMessage!),
+            );
+          } else {
+            return const SizedBox();
+          }
         },
       ),
     );
